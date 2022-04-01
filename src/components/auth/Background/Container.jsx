ab358705ea82626635e23414c6b4fbcba7215018
meriem <meriembenismail@insat.u-carthage.tn>
2022-03-31 22:12:01 -0700

    multi step form

--- src/components/auth/Background/Container.jsx
+++ src/components/auth/Background/Container.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import classes from './Container.module.css';
 import {PictureSection} from './PictureSection'
-import FormSection from './FormSection'
+import FormSection from './FormSection';
+import wavyLine from '../../../assets/wavyLine.svg';
+import { useState } from 'react';
 export const Container = () => {
+  
+
+  const [submitted, setSubmitted] = useState(false);
+  const onSubmitForm = () => {
+    setSubmitted(true);
+  };
+
   return (
     <div className={classes.authContainer}>
+        <div className={classes.authHead}>
+          <h2 className={classes.logo}>Trena</h2>
+          <img className={classes.wavyLine} src={wavyLine} alt="" />
+        </div>
         <div className={classes.authBody}>
             <div className={classes.pictureSection}>
                 <PictureSection />
             </div>
+            
             <div className={classes.formSection}>
-                <FormSection /> 
+                <FormSection onSubmitForm={onSubmitForm} submitted={submitted} /> 
             </div>
         </div>
 
--- src/components/auth/Background/PictureSection.jsx
+++ src/components/auth/Background/PictureSection.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Button } from '../../../UI/button/Button';
 import classes from './PictureSection.module.css';
 
-export const PictureSection = () => {
+export const PictureSection = (props) => {
   return (
     <div className={classes.picture}>
 
-        <div className={classes.pictureSection}></div>
-        
-
+        <div className={classes.pictureSection}>
+        <div className={classes.startBtn}>
+          <Button type="button" color="#005236" content="Commencer" onClick={props.onShow}/>
+        </div>
+        </div>
+ 
     </div>
     
   )
