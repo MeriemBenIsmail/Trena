5ba2e129c49469a3de9782156e9ab2f4dd86274d
meriem <meriembenismail@insat.u-carthage.tn>
2022-04-29 01:49:45 -0700

    navbar + terrain slider section with dummy data

--- src/components/home/TerrainsSlider.jsx
+++ src/components/home/TerrainsSlider.jsx
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './TerrainsSlider.module.css';
 import { Line } from '../../UI/line/Line';
 import terrains from '../../common/data/Terrains.json';
 import { TerrainCard } from '../terrain/TerrainCard';
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "./Swiper.css";
+import img1 from '../../assets/terrains/1.jpg'
+import { Button } from '../../UI/button/Button';
+
+
 export const TerrainsSlider = () => {
+
+    SwiperCore.use([EffectCoverflow, Pagination])
+    
   return (
     <div className={classes.terrainWrapper}>
          <div className={classes.title}>
             <Line color="#6BC4A6" />
             <h2>Terrains Sportifs</h2>
         </div>
-        <div className={classes.terrainSlider}>
+        <div className={classes.terrainSlider} style={{backgroundImage:`url(${img1})`}}>
             <div className={classes.container}>
-                <div className={classes.terrains}>
+                <div className={classes.btn}>
+                    <Button content="Voir Plus" color="#6BC4A6" />
+                </div>
+                <div >
+                <Swiper
+                autoplay={true}
+                loop={true}
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={"auto"}
+                    coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 6,
+                    slideShadows: false,
+                    }}
+                    className="mySwiper"
+                  
+                   
+                >
+        
+     
+  
                 {terrains.map((val,key) => {
-                    return <div className={classes.singleterrain}>
-                        <TerrainCard id={val.id} title={val.title} description={val.description} address={val.address} surface={val.surface} image={val.image} review={val.review}  />
+                    return <div >
+                        <SwiperSlide>
+                            <TerrainCard id={val.id} title={val.title} description={val.description} address={val.address} surface={val.surface} image={process.env.PUBLIC_URL + `/terrains/${val.id}.jpg`} review={val.review}  />
+                        </SwiperSlide>
+                        
                     </div> 
 
                 } )}
+                 </Swiper>
                 </div>
             </div>
            
