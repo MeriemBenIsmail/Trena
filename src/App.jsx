f6d6fe57faa90fd00a548883d8286c0277e828b6
seddik-houimli <Seddik.houimli@tum.de>
2022-05-31 16:06:42 +0100

    admin added

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Home }from "./pages/Home"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Terrain } from "./pages/Terrain";
 import { Coach } from "./pages/Coach";
+import { Admin } from "./pages/Admin"
 function App() {
   const {loading ,error ,user}= useUserContext();
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route exact path="/profil" element={<Profile></Profile> }/>
         <Route exact path="/terrains" element={<Terrain></Terrain> }/>
         <Route exact path="/coachs" element={<Coach></Coach> }/>
+        <Route exact path="/admin" element={<Admin></Admin> }/>
       </Routes>
     </Router>
    
