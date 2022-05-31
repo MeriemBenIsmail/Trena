92bfa95b95dea505016c9ffa5d819e6935ba6a88
meriem <meriembenismail@insat.u-carthage.tn>
2022-05-31 16:19:22 -0700

    commits

---
+++ src/components/terrain/Reservation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Reservation.module.css';
+
+function Reservation() {
+  return (
+    <div className={classes.reservation}>
+        <h2>Réservations</h2>
+
+
+    </div>
+  )
+}
+
+export default Reservation;
--- src/pages/Terrain.jsx
+++ src/pages/Terrain.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Navbar } from '../components/shared/Navbar'
 import Catalogue from '../components/terrain/Catalogue'
 import Steps from '../components/terrain/Steps'
+import { useUserContext } from "../contexts/userContext";
 
 export const Terrain = () => {
+  const { user, logoutUser } = useUserContext();
   return (
     <div>
         
-        <Steps />
+        <Steps user={user} logout={logoutUser} />
         <Catalogue />
     </div>
   )
